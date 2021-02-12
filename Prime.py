920bf94ce559c9f900fa1ac89b407b9b637f0ff0
sanzusahani98 <78102736+sanzusahani98@users.noreply.github.com>
2021-02-12 20:45:57 +0530

    Assignment 3
    Check weather no. Is prime

---
+++ Prime.py
@@ -0,0 +1,15 @@
+#Question 1
+#Write a function in Python to check whether a number is Prime or not.
+num = int(input("Enter a number: "))  
+  
+if num > 1:  
+   for i in range(2,num):  
+       if (num % i) == 0:  
+           print(num,"is not a prime number")  
+           print(i,"times",num//i,"is",num)  
+           break  
+   else:  
+       print(num,"is a prime number")  
+         
+else:  
+   print(num,"is not a prime number")
